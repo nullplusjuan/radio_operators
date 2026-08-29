fe1041419a16537095bd4bcd068d24e289c6a9b4
Joshua Dhalai <joshuadhalai@live.com>
2026-08-29 00:08:14 -0400

    Remove license declaration from manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,6 @@
 {
     "name": "Radio Operators",
     "version": "18.0.2.0.0",
-    "license": "AGPL-3",
     "summary": "Track radio operator callsigns on contacts",
     "description": """
 Radio Operators
